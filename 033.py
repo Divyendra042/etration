d393297e3fb8784e51df01b51bf4e562fdb73a68
Divyendra042 <115392336+Divyendra042@users.noreply.github.com>
2022-10-19 09:43:50 +0530

    Add files via upload

---
+++ 033.py
@@ -0,0 +1,12 @@
+basket = ["potato","tamato", "ginger","onion","capsicum","green chilli"]
+
+ 
+for item in basket :
+    print (item)
+
+mobileshop = ["iphone 13","samsung","oneplus nord","blackberry z10"]
+
+for item in mobileshop :
+    print(item)
+name = "chintu"  
+first_char=name[0]  
